fcbb618ffb66a33f8db7d272435fc545fbc32f28
suleka-mohamed <sulekamoha004@gmail.com>
2022-12-09 12:49:55 +0300

    added a new line

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
+/**
+ * puts 
+ * prints 
+ * on the
+ * screen*/
  int main(void)
 {
-	char Mystr[]="\"programming is like building a multilingual puzzle";
+	char Mystr[]="\n Programming is like building a multilingual puzzle";
 
  puts(Mystr);
  return(0);
