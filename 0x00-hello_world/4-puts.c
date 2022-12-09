3279ee91131c32b41615cf0c2a181fe09ad2c90d
suleka-mohamed <sulekamoha004@gmail.com>
2022-12-09 13:10:05 +0300

    corrected error

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -8,7 +8,7 @@
 **/
 int main(void)
 { 
-	char Mystr[] = "\n"Programming is like building a multilingual puzzle";
+	char Mystr[] = "\"Programming is like building a multilingual puzzle";
 puts(Mystr);
 return (0);
 }
