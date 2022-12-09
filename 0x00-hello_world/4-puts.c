47ef26e8ebae880b0cf4e4289bfa2ac456b02f79
suleka-mohamed <sulekamoha004@gmail.com>
2022-12-09 12:55:15 +0300

    correcting errors

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 /**
- * puts 
- * prints 
- * on the
- * screen*/
- int main(void)
+ *puts
+ *prints
+ *on the
+ *screen
+**/
+int main(void)
 {
-	char Mystr[]="\n Programming is like building a multilingual puzzle";
-
- puts(Mystr);
- return(0);
+	char Mystr[] = "\n Programming is like building a multilingual puzzle";
+puts( Mystr);
+return(0);
  }
