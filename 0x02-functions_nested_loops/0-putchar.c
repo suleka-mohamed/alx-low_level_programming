881344b285466fd68e7566ae3853904d375bce39
suleka-mohamed <sulekamoha004@gmail.com>
2022-12-14 18:09:21 +0300

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,10 @@
+#include <main.h>			
+/**
+ *-putchar -writes  the character c to stdout	*@c: the character to print		
+ *
+ * Return: zero
+ */
+int main(void)
+{char *_putchar="putchar"			while(*_putchar){				_putchar(*_putchar);				_putchar++;						}
+_putchar('\n');					return(0);
+}
