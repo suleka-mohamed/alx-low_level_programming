cd556e51143d8dab9a02783796e2abb380224010
suleka-mohamed <sulekamoha004@gmail.com>
2022-12-14 12:53:54 -0800

    betty documentation stylr

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,13 +1,5 @@
-#include "main.h"					/**							*print_alphabet -prints alphabets in lowercase		*							*Return: Zero						*/
+#include "main.h"					/**							*the function print_alphabet - prints alphabets in lowercase							*							*Return: Zero						*/
 void print_alphabet(void)
 {
-	int i;
-
-	for (i = 'a'; i <= 'z'; i++)
-	{
-		_putchar(i);
-		
-	}
-	_putchar('\n');
-	return;
-}
+	int i;							for (i = 'a'; i <= 'z'; i++)
+	{								_putchar(i);					}								_putchar('\n');				}
