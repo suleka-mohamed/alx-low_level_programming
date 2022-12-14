6331540154090aec5d0c676e1d4a00746b1bfcf9
suleka-mohamed <sulekamoha004@gmail.com>
2022-12-14 12:39:18 -0800

    updating

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,12 @@
+#include "main.h"					/**							*print_alphabet -prints alphabets in lowercase		*							*Return: Zero						*/
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = 'a',i <= 'z',i++)
+	{
+		_putchar(i);
+		_putchar('\n');	
+	}
+	return(0);
+}
