844ac0c89376a574f5566bb7cd4719bf03d4086f
suleka-mohamed <sulekamoha004@gmail.com>
2022-12-14 18:41:47 +0300

    including the #include

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,9 @@
-#include <main.h>			
+#include "main.h"			
 /**
  *-putchar -writes  the character c to stdout	*@c: the character to print		
  *
  * Return: zero
  */
-#include "main.h"
 int main(void)
 {_putchar('_';					_putchar('p'_;					_putchar('u');					_putchar('t');					_putchar('c');					_putchar('h');					_putchar("a');					_putchar("r");					_putchar('\n');				}						return(0);
 }
