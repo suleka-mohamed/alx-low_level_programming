d9983e1c4d3409aefe320d7613de90bf2de4c448
suleka-mohamed <sulekamoha004@gmail.com>
2022-12-14 19:07:35 +0300

    stdout using putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,6 +4,7 @@
  *
  * Return: zero
  */
-int main(void)
-{_putchar("_");					_putchar("p");					_putchar("u");					_putchar("t");					_putchar("c");					_putchar("h");					_putchar("a");					_putchar("r");					_putchar("\n");				}						return(0);
+int main(void){
+char *putchar = "_putchar";									while(*putchar){				_putchar(*putchar);				holberton++;
+}						_putchar('\n');					return(0);	
 };
